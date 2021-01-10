4b00128fb187f33d7042fc01f9106f0a2eec0429
Colin Shaw <colin.shaw2@uoit.net>
2021-01-10 15:31:40 -0500

    changed some sql scripts for testing

--- sqlScripts/testScripts.sql
+++ sqlScripts/testScripts.sql
@@ -1,5 +1,24 @@
 set schema 'quickqueue';
 select * FROM items;
+select * FROM users;
+select * FROM carts;
 
 insert into users  (username,"password",first_name,last_name,email,user_role)
-values ('kelvintrinh174','12345','Kelvin','Trinh','kelvintrinh174@gmail.com','CUSTOMER');
+values ('kelvintrinh174','12345','Kelvin','Trinh','kelvintrinh174@gmail.com','CUSTOMER'),
+('colinshaw2',	'c',	'Colin',	'Shaw',	'cs@gmail.com',	'SHOPPER');
+
+
+
+
+
+
+
+----------------------------------------------------------------------
+--testing for adding items
+
+insert into items values(1),(6);
+
+insert into carts (cart_customer_id, cart_shopper_id, cart_status)
+values (1, null, 'ACTIVE');
+
+select * FROM carts where (carts.cart_id = 1 and carts.cart_status = 'ACTIVE')
