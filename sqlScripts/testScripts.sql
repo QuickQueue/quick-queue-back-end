136f2e50f215022d0411188626a8541c761fb4b4
kelvintrinh174 <kelvintrinh174@gmail.com>
2021-01-10 10:48:18 -0500

    update script

--- sqlScripts/testScripts.sql
+++ sqlScripts/testScripts.sql
@@ -1,2 +1,5 @@
 set schema 'quickqueue';
 select * FROM users;
+
+insert into users  (username,"password",first_name,last_name,email,user_role)
+values ('kelvintrinh174','12345','Kelvin','Trinh','kelvintrinh174@gmail.com','CUSTOMER');
