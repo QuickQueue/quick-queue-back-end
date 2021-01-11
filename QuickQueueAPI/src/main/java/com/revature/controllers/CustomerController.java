6059b3eaad65ec4fa7fa43ded04ae9752a123f30
Colin Shaw <colin.shaw2@uoit.net>
2021-01-11 15:26:46 -0500

    items can be added to carts from a put request

--- QuickQueueAPI/src/main/java/com/revature/controllers/CustomerController.java
+++ QuickQueueAPI/src/main/java/com/revature/controllers/CustomerController.java
@@ -1,16 +1,21 @@
 package com.revature.controllers;
 
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.session.Session;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import com.revature.exceptions.UnauthenticatedException;
 import com.revature.models.Cart;
 import com.revature.models.User;
 import com.revature.services.ICartService;
@@ -28,17 +33,27 @@ public class CustomerController {
 		this.cartService = cartService;
 	}
 	
+	private static HttpSession getHttpSession() {
+		return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getSession();
+	}
+	
 	@PostMapping("/login") // curly braces denote it as a path variable -> when can extract the data
 	public ResponseEntity<User> logIn(@RequestBody User u){
-		return new ResponseEntity<User>(us.login(u.getUsername(), u.getPassword()), HttpStatus.OK);
+		HttpSession sess = getHttpSession();
+		u = us.login(u.getUsername(), u.getPassword());
+		sess.setAttribute("user", u);
+		return new ResponseEntity<User>((u), HttpStatus.OK);
 
 	}
 	
-	@PostMapping("/addItem/{itemid}/{quantity}/{userid}")
-	public ResponseEntity<Cart> addItem(@PathVariable int itemid, @PathVariable int quantity, @PathVariable int userid){
+	@PostMapping("/addItem/{itemid}/{quantity}")
+	public ResponseEntity<Cart> addItem(@PathVariable int itemid, @PathVariable int quantity){
 		//TODO check that user is logged in
-		//get session
-		
+		HttpSession sess = getHttpSession();
+		if(null == sess) {
+			throw new UnauthenticatedException();
+		}
+		int userid = ((User) sess.getAttribute("user")).getUserId();
 		return new ResponseEntity<Cart>(cartService.addItem(itemid, userid, quantity), HttpStatus.OK);
 
 	}
